2719d19d2bfd7013f28ab632acd9a3f5d928cd8a
cygairko <cygairko@gmail.com>
2019-11-12 16:24:16 +0100

    postgres12 update redis:5 nextcloud:17 nginx:1

--- backup.sh
+++ backup.sh
@@ -1,7 +1,24 @@
 
 
 ## backup postgres volume into tar
-docker run --rm --volumes-from postgres -v $(pwd):/backup alpine tar cvf /backup/bak_v_postgres11.tar /var/lib/postgresql/data
+docker run --rm --volumes-from postgres -v $(pwd):/backup alpine tar -cvf /backup/bak_v_postgres11.tar -C /var/lib/postgresql data
 
 ## restore
-docker run -v v_postgres11:/var/lib/postgresql/data -v $(pwd):/backup alpine tar xvf /backup/bak_v_postgres11.tar -C /var/lib/postgresql/data --strip 1
+docker run --rm -v v_postgres11:/var/lib/postgresql/data -v $(pwd):/backup alpine tar -xvf /backup/bak_v_postgres11.tar -C /var/lib/postgresql
+
+
+
+docker run --rm -it -v v_postgres11:/var/lib/postgresql/11/data -v v_postgres12:/var/lib/postgresql/12/data --name pgu postgres:12 #!/usr/bin/env bash
+
+## exec
+apt-get update && apt-get install postgresql-11
+chown -R postgres:postgres /var/lib/postgresql
+
+chown -R postgres:postgres /var/lib/postgresql
+
+su postgres
+initdb -D /var/lib/postgresql/12/data
+
+cd /var/lib/postgresql
+
+pg_upgrade -b /usr/lib/postgresql/11/bin -B /usr/lib/postgresql/12/bin -d /var/lib/postgresql/11/data -D /var/lib/postgresql/12/data -v
