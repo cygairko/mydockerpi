7fe6d959b750c83883744cd4dcd098a3d7985f5e
Hypriot Pirate <pirate@black-pearl.localdomain>
2019-11-16 00:40:40 +0100

    back to postgres11 on old volume

---
+++ pg_upgrade/update.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+# TODO scrape this somehow?
+supportedVersions=(
+	12
+	11
+	10
+	9.6
+	9.5
+	9.4
+	9.3
+	9.2
+)
+
+for i in "${!supportedVersions[@]}"; do
+	new="${supportedVersions[$i]}"
+	echo "# $new"
+	docker pull "postgres:$new" > /dev/null
+	(( j = i + 1 ))
+	for old in "${supportedVersions[@]:$j}"; do
+		dir="$old-to-$new"
+		echo "- $old -> $new ($dir)"
+		oldVersion="$(
+			docker run --rm -e OLD="$old" "postgres:$new" bash -Eeuo pipefail -c '
+				sed -i "s/\$/ $OLD/" /etc/apt/sources.list.d/pgdg.list
+				apt-get update -qq 2>/dev/null
+				apt-cache policy "postgresql-$OLD" \
+					| awk "\$1 == \"Candidate:\" { print \$2; exit }"
+			'
+		)"
+		echo "  - $oldVersion"
+		if [ "$oldVersion" = '(none)' ]; then
+			continue
+		fi
+		mkdir -p "$dir"
+		sed \
+			-e "s!%%POSTGRES_OLD%%!$old!g" \
+			-e "s!%%POSTGRES_OLD_VERSION%%!$oldVersion!g" \
+			-e "s!%%POSTGRES_NEW%%!$new!g" \
+			Dockerfile.template \
+			> "$dir/Dockerfile"
+		cp docker-upgrade "$dir/"
+		if [[ "$old" != 9.* ]]; then
+			sed -i '/postgresql-contrib-/d' "$dir/Dockerfile"
+		fi
+	done
+done
